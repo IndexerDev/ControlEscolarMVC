6ac6c9d92faa7e2d18e94f6d287105a362d79c16
ChrisMaister <cnavarro@indexersoft.com>
2018-07-26 03:05:23 -0500

    Integración de EF Repository
    Integra EF en DAL (sin separación de models) y el repositorio genérico

---
+++ Control Escolar/DAL/IRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public interface IRepository<Tentity> where Tentity : class
+    {
+        Tentity Get(int id);
+        IEnumerable<Tentity> GetAll();
+        IEnumerable<Tentity> Find(Expression<Func<Tentity, bool>> predicate);
+
+        void Add(Tentity entity);
+
+        void Update(Tentity entity);
+
+        void Remove(Tentity entity);
+
+        
+    }
+}
---
+++ Control Escolar/DAL/Repository.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        private readonly DbContext _context;
+
+        public Repository(DbContext context)
+        {
+            _context = context;
+        }
+
+        public void Add(TEntity entity)
+        {
+            _context.Set<TEntity>().Add(entity);
+            _context.SaveChanges();
+        }
+
+        public IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> predicate)
+        {
+            return _context.Set<TEntity>().Where(predicate);
+        }
+
+        public TEntity Get(int id)
+        {
+            return _context.Set<TEntity>().Find(id);
+        }
+
+        public IEnumerable<TEntity> GetAll()
+        {
+            return _context.Set<TEntity>().ToList();
+        }
+
+        public void Remove(TEntity entity)
+        {
+            _context.Set<TEntity>().Remove(entity);
+            _context.SaveChanges();
+        }
+
+        public void Update(TEntity entity)
+        {
+            _context.Entry(entity).State = EntityState.Modified;
+            _context.SaveChanges();
+        }
+        
+    }
+}
